7222ef926011a6f1f6be29738be1e6c6fd4b6049
Sebastian Schnichels <seb0305@gmx.de>
2025-11-17 14:26:18 +0100

    Implemented data manager

---
+++ data_manager.py
@@ -0,0 +1,29 @@
+from models import db, User, Movie
+
+class DataManager():
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def get_users(self):
+        return User.query.all()
+
+    def get_movies(self, user_id):
+        return Movie.query.filter_by(user_id=user_id).all()
+
+    def add_movie(self, movie):
+        db.session.add(movie)
+        db.session.commit()
+
+    def update_movie(self, movie_id, new_title):
+        movie = Movie.query.get(movie_id)
+        if movie:
+            movie.name = new_title
+            db.session.commit()
+
+    def delete_movie(self, movie_id):
+        movie = Movie.query.get(movie_id)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
